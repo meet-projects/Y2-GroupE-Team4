b82308140a28b270fed617432ae42f374dd26237
tamarha24-meet <139680727+tamarha24-meet@users.noreply.github.com>
2023-07-30 13:04:23 +0300

    Update app.py
    firebase setup

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 #Code goes below here
 
+config = {
+  "apiKey": "AIzaSyCQzrGhi1JdpqDhL_GfyXEfX52MbZkwKis",
+  "authDomain": "isha-9977d.firebaseapp.com",
+  "projectId": "isha-9977d",
+  "storageBucket": "isha-9977d.appspot.com",
+  "messagingSenderId": "1032310546572",
+  "appId": "1:1032310546572:web:34a33ede76ea79ec865ba9",
+  "measurementId": "G-YZ4HRFXQ9R"
+  "databaseURL": "https://isha-9977d-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 
