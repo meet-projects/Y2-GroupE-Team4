a239bef9601d89d0ad707cded93f6e96d1f7e00e
tamarha24-meet <139680727+tamarha24-meet@users.noreply.github.com>
2023-07-30 14:13:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,12 +43,16 @@ def about():
 def donate():
     if request.method == 'POST':
         try:
+            #need to add name attributes for each one & a donation display (in html) 
             email = request.form['email'].lower()
             name = request.form['name']
             amount = request.form['amount']
-            donation = {"email": email, "name": name, "amount": amount}
+            msg = request.form['msg']
+            display = request.form['display']
+            donation = {"email": email, "name": name, "amount": amount, "msg": msg, "display": display}
             db.child("Donations").push(donation)
-            return render_template("donate.html")
+            donations = db.child("Donations").get().val()
+            return render_template("donate.html", donations = donations)
 
         except:
             return render_template("donate.html")
